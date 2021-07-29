48b88c7dc0b22839625053eb4582b7c0f0694a66
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-07-29 13:25:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+let query = document.getElementById("text");
+
+function changeText() {
+    query.innerHTML = "This is really cool!"
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    changeText()
+  });
